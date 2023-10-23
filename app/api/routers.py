92acec12ec604eb265e3fa2018aec66e379c314d
Сергей Шлемин <schlyomin@yandex.ru>
2023-10-23 17:41:25 +0300

    final

--- app/api/routers.py
+++ app/api/routers.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.endpoints.user import router as user_router
 from app.api.endpoints.charity_project import router as charity_project_router
 from app.api.endpoints.donation import router as donation_router
+from app.api.endpoints.google_api import router as google_api_router
 
 main_router = APIRouter()
 
@@ -15,5 +16,10 @@ main_router.include_router(
     donation_router,
     prefix='/donation',
     tags=['Donation']
+)
+main_router.include_router(
+    google_api_router,
+    prefix='/google',
+    tags=['Google']
 )
 main_router.include_router(user_router)
